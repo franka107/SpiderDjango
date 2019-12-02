347e008a04272b53df1e8503f658213c225708b9
Frank Cary <fcaryv@gmail.com>
2019-12-02 17:58:13 -0500

    Modelado de datos v2

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class ReadAdmin(admin.ModelAdmin):
+    list_display = ('robot', 'sensor', 'date')
+
+class ValueAdmin(admin.ModelAdmin):
+    list_display = ('read' , 'value')
+
+admin.site.register(models.Robot)
+admin.site.register(models.Sensor)
+admin.site.register(models.Read, ReadAdmin)
+admin.site.register(models.Value, ValueAdmin)
+admin.site.register(models.Current_movement)
+admin.site.register(models.Past_movement)
--- main/models.py
+++ main/models.py
@@ -5,30 +5,41 @@ from django.contrib.auth.models import User
 class Sensor(models.Model):
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Robot(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    date_joined = models.TimeField()
+    date_joined = models.DateField()
     robot_type = models.CharField(max_length = 50)
     sensors = models.ManyToManyField(Sensor)
+
+    def __str__(self):
+        return self.name
 
 class Read(models.Model):
     robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+       return str(self.pk) + ' - ' +self.sensor.name + ' - ' + self.robot.name
 
 class Value(models.Model):
     read = models.ForeignKey(Read,  on_delete=models.CASCADE)
     value = models.IntegerField()
 
-class Current_movements(models.Model):
+    def __str__(self):
+        return  self.read
+class Current_movement(models.Model):
     robot = models.OneToOneField( Robot, on_delete=models.CASCADE)
     direction = models.CharField(max_length = 30)
 
-class Past_movements(models.Model):
+class Past_movement(models.Model):
     robot = models.ForeignKey( Robot, on_delete=models.CASCADE)
     direction = models.CharField(max_length = 30)
     runtime_date = models.DateTimeField() 
